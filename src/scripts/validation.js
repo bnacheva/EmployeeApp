7144451b9a3df7083d0cc68ad6010201d8d78d36
bnacheva <bori97@abv.bg>
2019-06-24 15:10:14 +0300

    Changes in the folder structure and adding some login form validation

---
+++ src/scripts/validation.js
@@ -0,0 +1,37 @@
+$(function validate() {
+	$("#login-form").validate({
+		rules: {
+			username: {
+				required: true,
+				minlength: 3,
+				maxlength: 10,
+				specialchars: true
+			},
+			password: {
+				required: true,
+				minlength: 6,
+				chars: true
+			}
+		},
+		messages: {
+			username: {
+				required: "Потребителското име е задължително поле.",
+				minlength: "Моля въведете поне 3 символа.",
+				maxlength: "Моля въведете най-много 10 символа.",
+				specialchars: "Моля въведете само букви, цифри и _."
+			},
+			password: {
+				required: "Паролата е задължително поле.",
+				chars: "Паролата трябва да съдържа задължително поне 1 главна, 1 малка буква и 1 цифра.",
+				minlength: "Паролата трябва да бъде поне 6 символа."
+			}
+		}
+	});
+
+	jQuery.validator.addMethod("specialchars", function (value, element) {
+		return this.optional(element) || /^[a-zA-Z0-9_]+$/i.test(value);
+	}, "Само букви, цифри и _.");
+	jQuery.validator.addMethod("chars", function (value, element) {
+		return this.optional(element) || /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/i.test(value);
+	}, "Паролата трябва да съдържа задължително поне 1 главна, 1 малка буква и 1 цифра.");
+});
